c452da3ede7680741f90e2dd59a7c753615e595f
Sam <wtawonlife@gmail.com>
2022-10-25 20:37:49 +0900

    🔧검색결과에서 상품 상세 정보 보기 오류 수정

--- src/pages/ProductSearch/SearchResult.jsx
+++ src/pages/ProductSearch/SearchResult.jsx
@@ -1,19 +1,25 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import styles from "../../style";
 
 function SearchResult({ listdata }) {
+  const navigate = useNavigate();
+  console.log(listdata);
   return (
     <section className={`${styles.flexCenter} ${styles.sectionLayout}`}>
       <ul className="w-full grid lg:grid-cols-[repeat(3,350px)] md:grid-cols-[repeat(3,300px)] sl:grid-cols-[repeat(3,220px)] sm:grid-cols-[repeat(2, 220px)] ss:grid-cols-[repeat(2,200px)] grid-cols-[repeat(2,150px)] gap-y-[50px] justify-between">
         {listdata.map((list, idx) => {
           return (
             <li key={list.product_id}>
-              <Link to={`/products/${list.product_id}`}>
-                <div
-                  className={`lg:w-[350px] lg:h-[350px] md:w-[300px] md:h-[300px] sl:w-[220px] sl:h-[220px] ss:w-[200px] ss:h-[200px] w-[150px] h-[150px] rounded-[10px] border-[1px] bg-center bg-cover`}
-                  style={{ backgroundImage: `url(${list.image})` }}
-                ></div>
-              </Link>
+              <div
+                onClick={() =>
+                  navigate(`/products/${list.product_id}`, {
+                    state: { product: list },
+                  })
+                }
+                className={`lg:w-[350px] lg:h-[350px] md:w-[300px] md:h-[300px] sl:w-[220px] sl:h-[220px] ss:w-[200px] ss:h-[200px] w-[150px] h-[150px] rounded-[10px] border-[1px] bg-center bg-cover cursor-pointer`}
+                style={{ backgroundImage: `url(${list.image})` }}
+              ></div>
+
               <p className="md:text-[16px] sm:text-[14px] text-[11px] text-subText font-spoqa">
                 {list.store_name}
               </p>
